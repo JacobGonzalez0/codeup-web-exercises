d9376403541db80297c205f645b6a6d026ae1582
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2021-01-08 14:33:56 -0600

    Added auto complete

--- js/weathermap.js
+++ js/weathermap.js
@@ -51,7 +51,7 @@ function changeBackground(city){
     
 }
 
-function geoCode(search){
+function geoCode(search, flag = 0){
     if(typeof search == null)return
 
     if(typeof search == "object"){// if we give it an array
@@ -84,20 +84,36 @@ function geoCode(search){
             } 
         })
         
-        $.ajax("https://api.mapbox.com/geocoding/v5/mapbox.places/" + input + ".json?access_token=" + MAPBOXAPI).done( (data,status)=>{
-        
-            changeBackground(data.features[0].text)
+        if(flag){ //for autocomplete search
+            $.ajax("https://api.mapbox.com/geocoding/v5/mapbox.places/" + input + ".json?access_token=" + MAPBOXAPI).done( (data,status)=>{
+                var array = []
+                data.features.forEach( location =>{
+                    array.push(location.text)
+                })
+                autocomplete(document.getElementById("searchBar"), array)
+            })
+
             
-            document.getElementById("title").innerHTML = data.features[0].text + " - 5 Day Forcast"
-            document.getElementById("searchBar").value = data.features[0].text 
-            currentLocation = data.features[0].center // sets up easy var to access
-            marker.setLngLat(data.features[0].center) //moves marker
-            map.flyTo({
-                center: data.features[0].center
-            }); // fly animation
-            get5Day(data.features[0].center) //get forcast
+        }else{
+            $.ajax("https://api.mapbox.com/geocoding/v5/mapbox.places/" + input + ".json?access_token=" + MAPBOXAPI).done( (data,status)=>{
         
-        })
+                changeBackground(data.features[0].text)
+                
+                document.getElementById("title").innerHTML = data.features[0].text + " - 5 Day Forcast"
+                document.getElementById("searchBar").value = data.features[0].text 
+                currentLocation = data.features[0].center // sets up easy var to access
+                marker.setLngLat(data.features[0].center) //moves marker
+                map.flyTo({
+                    center: data.features[0].center
+                }); // fly animation
+                get5Day(data.features[0].center) //get forcast
+            
+            })
+    
+        }
+        
+        
+
     }
 
     
@@ -185,10 +201,17 @@ function createCard(data){
 
 var currentLocation = [-98.4951, 29.4246]
 
+
+
 document.getElementById("searchBar").addEventListener('change', (e)=>{
     e.preventDefault()
     geoCode(e.target.value)
-})//
+})
+
+document.getElementById("searchBar").addEventListener('input', (e)=>{
+    geoCode(e.target.value, true)
+})
+
 
 
 document.getElementById("searchButton").addEventListener('click', (e)=>{
@@ -216,3 +239,96 @@ marker.on("dragend", (e)=>{
     geoCode(cords)
     //get5Day(cords)
 })
+
+
+function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        document.getElementById("autoList").appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+          /*check if the item starts with the same letters as the text field value:*/
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            b.setAttribute("class", "autocomplete-item");
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            a.appendChild(b);
+          }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          /*If the arrow DOWN key is pressed,
+          increase the currentFocus variable:*/
+          currentFocus++;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 38) { //up
+          /*If the arrow UP key is pressed,
+          decrease the currentFocus variable:*/
+          currentFocus--;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 13) {
+          if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (x) x[currentFocus].click();
+          }
+        }
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+}
