5abdba978d352bd29cd7e17eb0a4f1b2cb1e44bd
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2021-01-14 11:40:27 -0600

    Created user inferface for searching latest commit

--- js/promises.js
+++ js/promises.js
@@ -26,7 +26,13 @@ function getLatestCommits(username){
                     //we got our last commit
                     let dateSec = Date.parse(data[0].commit.author.date)//gets UNIX time        
                     let date = new Date(dateSec)//sets UNIX time
-                    console.log(date)
+                    generateUserInfo({
+                        date,
+                        username: data[0].author.login,
+                        avatar: data[0].author.avatar_url,
+                        repo: lastRepoUpdate,
+                        commit: data[0].commit.message
+                    })
                 })
 
             })
@@ -38,3 +44,74 @@ function getLatestCommits(username){
 
 getLatestCommits("jacobgonzalez0")
 
+function wait(ms){
+
+    return new Promise((res,rej)=>{
+        console.log("See you in " + (ms/1000).toFixed(1) + " seconds!")
+        setTimeout((
+            ()=>{
+                console.log("Hey im done! It took me " + (ms/1000).toFixed(1) + " seconds to get here.")
+                res(ms)
+            }
+        ),ms)
+       
+    })
+}
+
+//webpage stuff down here
+
+document.getElementById("searchButton").addEventListener("click", (e)=>{
+    e.preventDefault()
+    generateUserInfo(
+        getLatestCommits(document.getElementById("username").value)
+    )
+})
+
+document.getElementById("search").addEventListener("submit", (e)=>{
+    e.preventDefault()
+    generateUserInfo(
+         getLatestCommits(document.getElementById("username").value)
+    )
+    
+})
+
+function generateUserInfo(data){
+
+    let target = document.getElementById("data")
+    //target.innerHTML = ""
+
+    let row = document.createElement("row")
+    row.setAttribute("class", "row");
+
+    //setup columns
+    let avatar = document.createElement("img");
+    avatar.setAttribute("class", "col-2 ");
+    avatar.src = data.avatar;
+
+    let username = document.createElement("col");
+    username.setAttribute("class", "col");
+    username.innerHTML = data.username;
+
+    let lastRepo = document.createElement("col");
+    lastRepo.setAttribute("class", "col");
+    lastRepo.innerHTML = data.repo
+
+    let commit = document.createElement("col");
+    commit.setAttribute("class", "col");
+    commit.innerHTML = data.commit;
+
+    let date = document.createElement("col");
+    date.setAttribute("class", "col");
+    date.innerHTML = data.date
+
+    row.appendChild(avatar);
+    row.appendChild(username);
+    row.appendChild(lastRepo);
+    row.appendChild(commit);
+    row.appendChild(date);
+    //append to the target
+
+    target.appendChild(row)
+
+
+}
